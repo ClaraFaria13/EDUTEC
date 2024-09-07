8b866e8867f1265faddc2683988cb79d8246a473
ClaraFaria13 <maria.clara.faria@eaportal.org>
2024-09-07 16:18:29 -0300

    EDUTEC-5 ou 6

--- QUIZ/tela1.js
+++ QUIZ/tela1.js
@@ -61,3 +61,31 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Erro ao carregar dados do JSON:', error));
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("escuro");
+    const imagem = document.getElementById("img-clara");
+    const imagem2 = document.getElementById("img-clara2");
+
+    if (localStorage.getItem("darkMode") === "enabled") {
+      document.body.classList.add("escuro");
+      imagem.src = '../IMG/fundoes.png'; 
+      imagem2.src = '../IMG/fundoes2.png';
+  }
+
+  button.addEventListener("click", function() {
+      document.body.classList.toggle("escuro");
+
+      if (document.body.classList.contains("escuro")) {
+          localStorage.setItem("darkMode", "enabled");
+
+          imagem.src = '../IMG/fundoes.png'; 
+          imagem2.src = '../IMG/fundoes2.png';
+      } else {
+          localStorage.setItem("darkMode", "disabled");
+
+          imagem.src = '../IMG/fundo.png'; 
+          imagem2.src = '../IMG/fundo2.png';
+      }
+  });
+})
--- script.js
+++ script.js
@@ -2,35 +2,38 @@ function navegarPara(pagina) {
     window.location.href = pagina;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("escuro");
     const imagem = document.getElementById("img-clara");
     const imagem2 = document.getElementById("img-clara2");
     const imagem3 = document.getElementById("img-ani");
     const imagem4 = document.getElementById("img-ani2");
 
-    button.addEventListener("click", function() {
-      document.body.classList.toggle("escuro");
-
-      if (document.body.classList.contains("escuro")) {
+    if (localStorage.getItem("darkMode") === "enabled") {
+        document.body.classList.add("escuro");
         imagem.src = '../IMG/fundoes.png'; 
         imagem2.src = '../IMG/fundoes2.png';
         imagem3.src = '../IMG/animaises.png'; 
         imagem4.src = '../IMG/animaises2.png';
-      } else {
-        imagem.src = '../IMG/fundo.png'; 
-        imagem2.src = '../IMG/fundo2.png';
-        imagem3.src = '../IMG/animais.png'; 
-        imagem4.src = '../IMG/animais2.png';
-      }
+    }
+
+    button.addEventListener("click", function() {
+        document.body.classList.toggle("escuro");
+
+        if (document.body.classList.contains("escuro")) {
+            localStorage.setItem("darkMode", "enabled");
+
+            imagem.src = '../IMG/fundoes.png'; 
+            imagem2.src = '../IMG/fundoes2.png';
+            imagem3.src = '../IMG/animaises.png'; 
+            imagem4.src = '../IMG/animaises2.png';
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+
+            imagem.src = '../IMG/fundo.png'; 
+            imagem2.src = '../IMG/fundo2.png';
+            imagem3.src = '../IMG/animais.png'; 
+            imagem4.src = '../IMG/animais2.png';
+        }
     });
-  });
-
-document.addEventListener("DOMContentLoaded", function() {
-  var hamburger = document.getElementById("hamburger");
-  var menuItems = document.getElementById("menu-items");
-  var configButton = document.getElementById("Ver-botoes");
-  var darkModeButton = document.getElementById("escuro");
-});
-
-
+})
